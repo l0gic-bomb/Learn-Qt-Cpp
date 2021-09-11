a4abe9109e5d4b4184498fb7e212ff906eb3045a
Artem Krymov <artem.krymov@bk.ru>
2021-09-11 10:05:08 +0300

    qtActivity add recyclerView

--- app/src/main/java/com/example/practicefragment/QtActivity.java
+++ app/src/main/java/com/example/practicefragment/QtActivity.java
@@ -2,11 +2,16 @@ package com.example.practicefragment;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.OrientationHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.practicefragment.models.RecyclerDataModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
@@ -40,6 +45,51 @@ public class QtActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        /**
+         * RecyclerView состоит следующим образом
+         *  название уровня 1
+         *  1. уровень
+         *  ...
+         *  n. уровень
+         *  название уровня 2
+         *  ...
+         */
+
+        String _nameLevel;
+        String _names[], _descs[];
+
+
+        RecyclerDataModel recyclerDataModel = new RecyclerDataModel();
+
+        _nameLevel = "JUNIOR";
+        _names = getResources().getStringArray(R.array.qt_name_level_junior);
+        _descs = getResources().getStringArray(R.array.qt_junior_description);
+        recyclerDataModel.setListData(_nameLevel, _names, _descs);
+
+        _nameLevel = "MIDDLE";
+        _names = getResources().getStringArray(R.array.qt_name_level_middle);
+        _descs = getResources().getStringArray(R.array.qt_middle_description);
+        recyclerDataModel.setListData(_nameLevel, _names, _descs);
+
+        _nameLevel = "SENIOR";
+        _names = getResources().getStringArray(R.array.qt_name_level_senior);
+        _descs = getResources().getStringArray(R.array.qt_senior_description);
+        recyclerDataModel.setListData(_nameLevel, _names, _descs);
+
+
+        DifferentRowAdapter adapter = new DifferentRowAdapter(recyclerDataModel.getData(), this);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, OrientationHelper.VERTICAL, false);
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_cpp);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(adapter);
     }
 
 }
