1e707e30b731de66ac339c83132a2c5f956da1c9
Artem Krymov <artem.krymov@bk.ru>
2022-08-10 22:57:08 +0300

    some fix

--- app/src/main/java/com/example/practicefragment/CppActivity.java
+++ app/src/main/java/com/example/practicefragment/CppActivity.java
@@ -10,14 +10,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.practicefragment.models.RecyclerDataModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 /**
@@ -73,17 +79,72 @@ public class CppActivity extends AppCompatActivity {
 
         String _nameLevel;
         String _names[], _descs[];
-
-        AssetManager am = getAssets();
-        InputStream is = null;
-        String result;
+        //AssetManager am = CppActivity.this.getAssets();
+        FileInputStream file = null;
         try {
-            is = am.open("/windows/cpp.json");
-            Scanner s = new Scanner(is).useDelimiter("\\windows\\cpp.json");
-            result = s.hasNext() ? s.next() : "";
-        } catch (IOException e) {
+            file = new FileInputStream("src/main/assets/cpp.json");
+            BufferedReader reader = new BufferedReader(file, file.);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        /* TO DO
+        1. Make to ContentReaderJson
+        2. Save to List<LevelEvent>
+         JSONObject object = new JSONObject();
+        object.put("name", "mkyong.com");
+        object.put("age", new Integer(100));
+
+        JSONArray list = new JSONArray();
+        list.add("msg 1");
+        list.add("msg 2");
+        list.add("msg 3");
+
+        object.put("messages", list);
+
+        try{
+            FileWriter file = new FileWriter("test.json");
+            file.write(object.toJSONString());
+            file.flush();
+            file.close();
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+
+        //============================================
+
+        // остановить перед чтением файла
+        JSONParser parser = new JSONParser();
+
+        try {
+            Object obj = parser.parse(new FileReader("test.json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            String name = (String) jsonObject.get("name");
+            System.out.println(name);
+
+            long age = (Long) jsonObject.get("age");
+            System.out.println(age);
+
+            // loop array
+            JSONArray msg = (JSONArray) jsonObject.get("messages");
+            Iterator<String> iterator = msg.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
+         */
 
 
         RecyclerDataModel recyclerDataModel = new RecyclerDataModel();
