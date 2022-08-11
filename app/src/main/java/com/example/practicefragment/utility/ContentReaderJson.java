6f800cd2bae5f9dfb59dba2f905c998e97cb162f
Artem Krymov <artem.krymov@bk.ru>
2022-08-11 21:25:26 +0300

    added method getDataFromFile
    added method getDataFromFile and wrtie data in jsonobjec

--- app/src/main/java/com/example/practicefragment/utility/ContentReaderJson.java
+++ app/src/main/java/com/example/practicefragment/utility/ContentReaderJson.java
@@ -1,13 +1,21 @@
 package com.example.practicefragment.utility;
 
+import android.content.Context;
+
+import com.example.practicefragment.models.RecyclerDataModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class ContentReaderJson {
     private static ContentReaderJson readerJson;
-    private String pathToFile;
-    private String dataFromFile;
+    private JSONObject object;
 
     //! singelton
     public static ContentReaderJson getReaderJson()
@@ -18,37 +26,42 @@ public class ContentReaderJson {
         return readerJson;
     }
 
-    //! setterPath
-    public void setToPath(String path)
-    {
-        pathToFile = path;
-        getDataFromFile();
-    }
-
     //! get all data from file
-    private void getDataFromFile()
+    public boolean getDataFromFile(String path, Context context)
     {
-        File file = new File(pathToFile);
-        Scanner scanner;
+        if (path.isEmpty())
+            return false;
+
+        object = null;
+        InputStream is = null;
         try {
-            scanner = new Scanner(file);
-            while (scanner.hasNextLine())
-            {
-                dataFromFile += scanner.nextLine();
-            }
-        } catch (FileNotFoundException e) {
+            String json = new String();
+            is = context.getAssets().open(path);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+            object = new JSONObject(json);
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
+            return false;
         }
+
+        if (object == null)
+            return false;
+
+        return true;
     }
 
-    //! get array from our file
-    public String[] getStringArray(String key)
+
+    //! get DataModel for every levels by key
+    public RecyclerDataModel getGeneralDataLevels(String key)
     {
-        if (dataFromFile == null || key == null)
+        if (object != null)
             return null;
 
-        String[] array = new String[1];
-        return array;
+        return new RecyclerDataModel();
     }
 
 
